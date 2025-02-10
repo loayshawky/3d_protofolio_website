0e3c38df592c5375bfe9998fc44079658a4a1b7e
loay shawky <105853863+loayshawky@users.noreply.github.com>
2025-02-10 15:33:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(async ({ command }) => {
   const plugins = [react()];
 
   if (command === 'serve') {
-    const visualizer = require('rollup-plugin-visualizer');
+    const { default: visualizer } = await import('rollup-plugin-visualizer');
     plugins.push(visualizer({ open: false }));
   }
 
@@ -16,7 +16,7 @@ export default defineConfig(({ command }) => {
     },
     build: {
       outDir: 'dist',
-      minify: true, // استخدم esbuild بدلاً من terser
+      minify: true, // استخدام esbuild بدلاً من terser
       treeShaking: true,
       assetsInlineLimit: 4096,
       chunkSizeWarningLimit: 500,
